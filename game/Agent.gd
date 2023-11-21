666a7efaaa86673965057b8c9ab8fc4c1d45a2a4
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2023-11-21 14:41:52 +0700

    pathfinding incomplete

--- game/Agent.gd
+++ game/Agent.gd
@@ -1,2 +1,9 @@
-extends CharacterBody2D
+extends AStar2D
 
+var id_to_grid : Dictionary
+
+func _compute_cost(from_id, to_id):
+	return 1
+	
+func _estimate_cost(from_id, to_id):
+	id_to_grid[from_id].distance_to(id_to_grid[to_id]) 
