cfc90f220f2d80ab357d3d17591c3b34675350b7
DESKTOP-QKDP7OI\manhh <manhhieu012@gmail.com>
2023-12-04 14:44:46 +0700

    animation

--- game/Agent.gd
+++ game/Agent.gd
@@ -1,7 +1,7 @@
-var id_to_grid : Dictionary
-
-func _compute_cost(from_id, to_id):
-	return 1
-	
-func _estimate_cost(from_id, to_id):
-	id_to_grid[from_id].distance_to(id_to_grid[to_id]) 
+#var id_to_grid : Dictionary
+#
+#func _compute_cost(from_id, to_id):
+#	return 1
+#
+#func _estimate_cost(from_id, to_id):
+#	id_to_grid[from_id].distance_to(id_to_grid[to_id]) 
